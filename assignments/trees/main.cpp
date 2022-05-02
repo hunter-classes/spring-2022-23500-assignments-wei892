c18a02fc3f6cdc9623b5866fdb2a8d8f947c5d63
Wei <wlin.li2403@gmail.com>
2022-05-02 01:57:17 -0400

    added main

---
+++ assignments/trees/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "Node.h"
+#include "Tree.h"
+#include <time.h>
+
+int main(){
+  srand(time(NULL));
+  Tree *t = new Tree();
+  int counter = 10;
+  while (counter > 0){
+    t->insert(rand() % 100 +1);
+    counter--;
+  }
+  
+  std::cout << t->printTree_Rec() << std::endl;
+  //std::cout << "printed";
+  int v = 15;
+  
+  try {
+    int x  = t->search(v);
+    std::cout << "Found: " << x << "\n";
+  } catch (int e){
+    std::cout << v << " not found\n";
+    
+  }
+  return 0;
+}
